21029ecdab31385fb8bce7e5e7ec74b7168955ce
pontusnilsson <pontus.nilsson@wunderkraut.se>
2017-03-25 17:14:33 +0100

    Update tests

--- web/modules/custom/one_two/tests/src/Functional/BrowserTest.php
+++ web/modules/custom/one_two/tests/src/Functional/BrowserTest.php
@@ -10,10 +10,36 @@ use Drupal\Tests\BrowserTestBase;
  * @group one_two
  */
 class BrowserTest extends BrowserTestBase {
+  protected $user;
+  public static $modules = ['block', 'node', 'datetime'];
 
+  /**
+   * @inheritdoc
+   */
+  protected function setUp() {
+
+    parent::setUp();
+
+    $this->drupalCreateContentType(['type' => 'page', 'name' => 'Basic page']);
+    $this->user = $this->drupalCreateUser([
+      'edit own page content',
+      'create page content'
+    ]);
+
+    $this->drupalPlaceBlock('local_tasks_block');
+  }
+
+  /**
+   * Test.
+   */
   function testDrupalGet() {
     $this->drupalGet('user/register');
+
     $this->assertSession()->pageTextContains('Create new account');
+    $this->assertSession()->fieldExists('Email address');
+    $this->assertSession()->fieldExists('Username');
+    $this->assertSession()->buttonExists('Create new account');
+    $this->assertSession()->pageTextNotContains('Joomla');
   }
 
 }
